ca5fa0c93e34b13b764769255605ef4c89246664
Daniel Gehlen <harmonia251251@gmail.com>
2025-01-06 21:29:29 -0300

    Estilo melhorado

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-document.getElementById('formContato').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Mensagem enviada com sucesso! Entraremos em contato em breve.');
-    document.getElementById('formContato').reset();
+// Popup de saída
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault();
+    e.returnValue = 'Tem certeza que deseja sair?';
+});
+
+// Acordeões
+document.querySelectorAll('.neon-card').forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('active');
+    });
 });
