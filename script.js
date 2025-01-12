52a9262e653619e22af2e3bad0ab48a251e1ed6d
Daniel Gehlen <harmonia251251@gmail.com>
2025-01-12 14:19:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,35 +20,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Função para adicionar tópicos à sidebar
-    function addTopicsToSidebar() {
-        const topicList = document.getElementById('topic-list');
-        const topics = [
-            "Análise Musical",
-            "História da Música",
-            "Educação de Jovens e Adultos no Brasil: História e Política",
-            "Educação Musical na Infância",
-            "Fundamentos da Música",
-            "História da Música Brasileira",
-            "Harmonia",
-            "Instrumento - Canto",
-            "Instrumento - Violão",
-            "Materiais Didáticos",
-            "Prática Musical em Conjunto",
-            "Práticas de Composição para Educação Musical",
-            "Práticas Vocais para a Educação Musical",
-            "Projetos Sociais e Culturais e Educação Musical",
-            "Sociologia e Educação Musical",
-            "Tecnologias para Educação Musical"
-        ];
-
-        topics.forEach(topic => {
-            const li = document.createElement('li');
-            li.textContent = topic;
-            topicList.appendChild(li);
-        });
-    }
-
     // Função para filtrar posts por data
     function filterPostsByDate() {
         const hash = window.location.hash.substring(1);
@@ -64,27 +35,8 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    // Função para filtrar posts por tópico
-    function filterPostsByTopic(event) {
-        const query = event.target.value.toLowerCase();
-        const posts = document.querySelectorAll('.card.neon-card');
-
-        posts.forEach(post => {
-            const topic = post.querySelector('.post-topic').textContent.toLowerCase();
-            if (topic.includes(query)) {
-                post.style.display = 'block';
-            } else {
-                post.style.display = 'none';
-            }
-        });
-    }
-
-    // Adicionar datas e tópicos à sidebar
+    // Adicionar datas à sidebar
     addDatesToSidebar();
-    addTopicsToSidebar();
-
-    // Adicionar evento de filtro por tópico
-    document.getElementById('search-bar').addEventListener('input', filterPostsByTopic);
 
     // Filtrar posts por data ao carregar a página
     filterPostsByDate();
